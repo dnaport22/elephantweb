542266551bad58cb985afb32fbdeba9bdd738cde
deep-nav <navdeep.dhuti@cameronandwilding.com>
2016-12-31 14:20:11 +0000

    Dynamic dns

--- www/js/data.js
+++ www/js/data.js
@@ -1,21 +1,21 @@
 elephant.constant('elephantData_URL', {
-  GET_ALL_ITEM_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/getitems.php',
+  GET_ALL_ITEM_URL: 'http://'+getUrl(window.location.hostname)+'/getitems.php',
   GET_ALL_ITEM_TYPE: 'GET',
-  LOGIN_USER_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/login.php',
+  LOGIN_USER_URL: 'http://'+getUrl(window.location.hostname)+'/login.php',
   LOGIN_USER_TYPE: 'POST',
-  REGISTER_USER_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/register.php',
+  REGISTER_USER_URL: 'http://'+getUrl(window.location.hostname)+'/register.php',
   REGISTER_USER_TYPE: 'POST',
-  GET_USER_ITEM_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/myitems.php',
+  GET_USER_ITEM_URL: 'http://'+getUrl(window.location.hostname)+'/myitems.php',
   GET_USER_ITEM_TYPE:'GET',
-  DELETE_USER_ITEM_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/dismiss.php',
+  DELETE_USER_ITEM_URL: 'http://'+getUrl(window.location.hostname)+'/dismiss.php',
   DELETE_USER_ITEM_TYPE: 'POST',
-  POST_ITEM_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/postitem.php',
+  POST_ITEM_URL: 'http://'+getUrl(window.location.hostname)+'/postitem.php',
   POST_ITEM_TYPE: 'POST',
-  REQUEST_ITEM_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/message.php',
+  REQUEST_ITEM_URL: 'http://'+getUrl(window.location.hostname)+'/message.php',
   REQUEST_ITEM_TYPE: 'POST',
-  RESET_REQUEST_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/forgotpass_request.php',
+  RESET_REQUEST_URL: 'http://'+getUrl(window.location.hostname)+'/forgotpass_request.php',
   RESET_REQUEST_TYPE: 'POST',
-  RESET_VERIFY_URL: 'http://'+getUrl(window.location.host)+'.myelephant.xyz/forgotpass_verify.php',
+  RESET_VERIFY_URL: 'http://'+getUrl(window.location.hostname)+'/forgotpass_verify.php',
   RESET_VERIFY_TYPE: 'POST'
 })
 
@@ -44,14 +44,24 @@ elephant.constant('elephantData_RESETPASS', {
   RESET_PASS_VALIDATE: 'reset_pass2'
 })
 
+/**
+ * Return backend dns based on the current server of the user.
+ *
+ * @param location string
+ *
+ * @return dns string 
+ */
 function getUrl(location) {
   if (location == "developweb.myelephant.xyz") {
-    return 'develop';
+    return 'develop.myelephant.xyz';
   }
   else if (location == "testweb.myelephant.xyz") {
-    return 'test';
+    return 'test.myelephant.xyz';
   }
   else if (location == "myelephant.xyz") {
-    return 'service';
+    return 'service.myelephant.xyz';
+  }
+  else if (location == "localhost") {
+    return 'elephant.local';
   }
 }
