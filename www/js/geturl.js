3364d9c9d08b3d7234e44935df2cfddb7ee7f73e
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-01-02 15:19:03 +0000

    Adding seprate file for serving urls

---
+++ www/js/geturl.js
@@ -0,0 +1,22 @@
+
+/**
+ * Return backend dns based on the current server of the user.
+ *
+ * @param location string
+ *
+ * @return dns string 
+ */
+function getUrl(location) {
+  if (location == "developweb.myelephant.xyz") {
+    return 'develop.myelephant.xyz';
+  }
+  else if (location == "testweb.myelephant.xyz") {
+    return 'test.myelephant.xyz';
+  }
+  else if (location == "myelephant.xyz") {
+    return 'service.myelephant.xyz';
+  }
+  else if (location == "localhost") {
+    return 'elephant.local';
+  }
+}
